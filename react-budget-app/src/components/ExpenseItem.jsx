b9790402f16fabcfa1551a4f28410ea3de3f0a85
mirinae700 <mirinae219@gmail.com>
2025-02-06 00:19:51 +0900

    refactor: ExpenseItem css 추가, 금액 콤마 표시 및 지출의 경우 (-) 표시되도록 수정

--- react-budget-app/src/components/ExpenseItem.jsx
+++ react-budget-app/src/components/ExpenseItem.jsx
@@ -1,14 +1,20 @@
+import "./ExpenseItem.css";
 import { Component } from "react";
 import { MdEdit } from "react-icons/md";
 import { MdDeleteForever } from "react-icons/md";
 
 export default class ExpenseItem extends Component {
   render() {
+    const amount = this.props.expense.amount;
     return (
       <li className="item">
         <div className="info">
-          <span className="expense">{this.props.expense.content}</span>
-          <span className="amount">{this.props.expense.amount}원</span>
+          <span className="content">{this.props.expense.content}</span>
+          <span className="amount">
+            {this.props.expense.typeCode === "income"
+              ? `${amount.toLocaleString()}원`
+              : `-${amount.toLocaleString()}원`}
+          </span>
         </div>
         <div>
           <button className="btn edit-btn">
